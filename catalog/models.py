2475dd8743f9d375f4a6667074bcece642656f5b
GIO <beria.giorgi1@gmail.com>
2020-08-08 08:32:10 +0400

    qwe

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+STATUS_CHOICES = (
+    (1, "in library"),
+    (2, "taken")
+)
+
 class Author (models.Model):
     id=models.IntegerField(primary_key=True,null=False)
     name=models.CharField(max_length=255)
@@ -14,7 +19,7 @@ class Book (models.Model):
     id=models.IntegerField(primary_key=True,null=False)
     name=models.CharField(max_length=255)
     author=models.OneToOneField(Author,db_column="author_id",on_delete=models.CASCADE)
-    status=models.IntegerField()
+    status=models.IntegerField(choices=STATUS_CHOICES,default=1)
     takenBy=models.CharField(max_length=255)
     taken_at=models.DateTimeField(null=True,blank=True)
     returned_at=models.DateTimeField(null=True,blank=True)
