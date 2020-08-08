eb3dfd4e3e4e469f6df670ba43c933fc0caef747
Giorgi Beria <beria.giorgi1@gmail.com>
2020-08-08 16:53:59 +0400

    added front page +changed strftime issue

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,12 +3,19 @@ from catalog import models
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
+    fields = ('name', 'author', 'status', 'takenBy', 'taken_at', 'returned_at')
     list_display = ('name', 'author','status','takenBy','taken_at','returned_at')
     ordering = ('name',)
     search_fields = ('name', 'takenBy')
 
 
 admin.site.register(models.Book,BookAdmin)
+
+class AuthorAdmin(admin.ModelAdmin):
+    fields = ('name',)
+    list_display = ('name',)
+    ordering = ('name',)
+    search_fields = ('name',)
 
 
 admin.site.register(models.Author)
