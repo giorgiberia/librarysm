cf80a6af2c193111168d24e012f1df532a6d5e02
GIO <beria.giorgi1@gmail.com>
2020-08-13 22:31:12 +0400

    added local postgre and category backend

--- library/settings.py
+++ library/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '1517',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 import dj_database_url
 db_from_env=dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
